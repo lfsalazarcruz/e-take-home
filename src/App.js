d8247f01526925f7fdb92e7343cd15ce234294c6
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-03-22 21:46:27 -0700

    Created gifs container component. Working on fetching the data from giphy.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import GifsContainer from "./components/gifscontainer/GifsContainer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <GifsContainer />
       </div>
     );
   }
---
+++ src/components/gifscontainer/GifsContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class GifsContainer extends Component {
+  render() {
+    return <div className="gifs-container">This is the gifs container</div>;
+  }
+}
+
+export default GifsContainer;
