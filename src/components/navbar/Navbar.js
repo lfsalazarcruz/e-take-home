6df703adb196eb840742fe2087b306bb4352f6a2
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-03-24 16:19:31 -0700

    Worked on styling the gif cards and navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,9 +6,12 @@ class Navbar extends Component {
   render() {
     return (
       <div className="navbar-container">
-        {/* <h3 className="navbar-subtitle">welcome to </h3> */}
-        <h1 className="navbar-title">Gifr</h1>
-        <Searchbar />
+        <div className="navbar-inner-container">
+          {/* <h3 className="navbar-subtitle">welcome to </h3> */}
+          <h1 className="navbar-title">Gifr</h1>
+          <Searchbar />
+        </div>
+        <div className="navbar-divider" />
       </div>
     );
   }
