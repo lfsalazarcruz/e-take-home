9490cbabe4b9ed82f8007e53361d79f95a683406
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-03-23 18:18:47 -0700

    Added functionality to input field. Working on search functionality.
    App deployed to netlify

--- src/components/navbar/searchbar/Searchbar.js
+++ src/components/navbar/searchbar/Searchbar.js
@@ -2,15 +2,29 @@ import React, { Component } from "react";
 import "./Searchbar.css";
 
 class SearchBar extends Component {
+  state = {
+    giftitle: ""
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+    console.log("search for this title: ", this.state.giftitle);
+  };
+
   render() {
     return (
       <form className="searchbar-container">
         <input
           className="search-input"
-          value="Look for the best trending GIFs!"
+          placeholder="Look for the best trending GIFs!"
           type="text"
+          onChange={this.handleChange}
+          name="giftitle"
         />
-        <button className="search-button">search</button>
+        <button className="search-button" type="submit">
+          search
+        </button>
       </form>
     );
   }
