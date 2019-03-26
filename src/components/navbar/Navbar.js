5cf1ff2aaa134eacf6b88074d24ea9579ae9c442
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-03-26 12:29:57 -0700

    Updated design.

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -18,18 +18,21 @@ class Navbar extends Component {
   render() {
     return (
       <div className="navbar-container">
-        <Link style={{ textDecoration: "none", color: "white" }} to={`/`}>
-          <h1 className="navbar-title">Gifr</h1>
-        </Link>
-        <div className="searchbar-container">
-          <input
-            className="search-input"
-            placeholder="Look for the best trending GIFs!"
-            type="text"
-            onChange={event => this.onInputChange(event.target.value)}
-            name="term"
-          />
+        <div className="navbar-inner-container">
+          <Link style={{ textDecoration: "none", color: "white" }} to={`/`}>
+            <h1 className="navbar-title">Gifr</h1>
+          </Link>
+          <div className="searchbar-container">
+            <input
+              className="search-input"
+              placeholder="Look for the best trending GIFs!"
+              type="text"
+              onChange={event => this.onInputChange(event.target.value)}
+              name="term"
+            />
+          </div>
         </div>
+        <div className="navbar-divider" />
       </div>
     );
   }
